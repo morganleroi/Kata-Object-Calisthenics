b8d7683d1ac4610db9d482158c3f9b59defa11d6
momo3038 <morgan.leroi@gmail.com>
2016-06-14 22:17:51 +0200

    Add bootstrap for VS

---
+++ BankAccountKata/BankAccountKata.Test/BankAccountTest.cs
@@ -0,0 +1,14 @@
+﻿using NFluent;
+using NUnit.Framework;
+
+namespace BankAccountKata.Test
+{
+    public class BankAccountTest
+    {
+        [Test]
+        public void MyFirstTest()
+        {
+            Check.That(true).IsTrue();
+        }
+    }
+}
